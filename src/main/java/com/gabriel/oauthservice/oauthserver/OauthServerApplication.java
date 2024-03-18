3a96e3cf247187267e49e9fea6c254c7185fba12
Gabriel <gabrielvenegas@comercialpau.cl>
2024-03-18 12:20:01 -0300

    Setting DB and JPA

---
+++ src/main/java/com/gabriel/oauthservice/oauthserver/OauthServerApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.oauthservice.oauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServerApplication.class, args);
+	}
+
+}
